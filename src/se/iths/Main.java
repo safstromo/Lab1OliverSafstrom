c7d00362a3fed96827ce903caf4b309688421646
Oliver <safstrom.oliver@gmail.com>
2022-09-04 11:17:01 +0200

    add minMax, add sorting

--- src/se/iths/Main.java
+++ src/se/iths/Main.java
@@ -1,13 +1,17 @@
 package se.iths;
 
+import java.sql.SQLOutput;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(menu());
+        //System.out.println(menu());
 
-
+        int[] userData = new int[]{2, 2, 23, 5, 6, 3,1,0,0,0, 7};//userInput();
+        minMax(userData);
+        //sortering(userData);
     }
 
 
@@ -55,5 +59,125 @@ public class Main {
             }
         }
         return input;
+    }
+
+    public static int[] userInput() {
+//        Senaste året har elpriserna blivit högre och varierar mycket. Det här programmet ska kunna hjälpa till
+//        med att analysera elpriser för ett dygn. När man väljer alternativet inmatning från menyn ska
+//        programmet fråga efter priserna under dygnets timmar. Inmatningen av värden ska ske med hela
+//        ören. Tex kan priser vara 50 102 eller 680 öre per kW/h. Priset sätts per intervall mella
+//        två hela
+//        timmar. Dygnets första pris är då mellan 00-01, andra intervallet är mellan 01-02 osv
+
+        // Måste lägga till felhandtering för out of bounds. samt flytta saker till metoder.
+//
+
+        // Creates a new array and asks user for data.
+
+        Scanner sc = new Scanner(System.in);
+        int[] newUserData = new int[22];
+
+
+        while (true) {
+            System.out.println("Ange vilken timme vill du lägga in pris för? (00,01,02 osv.)");
+            int hour = sc.nextInt();
+
+            System.out.println("Ange priset för timme " + hour + "( ange i hela ören.)");
+            newUserData[hour - 1] = sc.nextInt();
+
+            System.out.println("Vill du ange fler priser? (Y/N)");
+            String morePrices = sc.next().toUpperCase();
+
+            if (morePrices.equals("Y"))
+                continue;
+            else if (morePrices.equals("N"))
+                break;
+            else {
+                System.out.println("Fel input (Y/N) du åker tillbaka till menyn");
+                break;
+            }
+        }
+        return newUserData;
+    }
+
+   /* private static int[] userData() {
+        int[] prices = new int[22];
+        return prices;
+    }*/
+
+    private static int[] testUserData() {
+        int[] prices = new int[]{1, 2, 3, 4, 5, 6};
+        return prices;
+    }
+
+    public static void minMax(int[] userData) {
+//        När alternativ 2 väljs på menyn så ska programmet skriva ut lägsta priset, högsta priset samt vilka
+//        timmar som detta infaller under dygnet. Dygnets medelpris ska också räknas fram och presenteras på
+//        skärmen.
+
+        // Flytta saker till egna methoder.
+
+        //Import userData and find min,max and average prices.
+
+        int minPrice = 0;
+        int maxPrice = 0;
+        double averagePrice = 0;
+
+        for (int i = 0; i < userData.length; i++) {
+            if (userData[i] != 0) {
+                minPrice = Arrays.stream(userData).min().getAsInt();
+                maxPrice = Arrays.stream(userData).max().getAsInt();
+                averagePrice = Arrays.stream(userData).average().getAsDouble();
+            }
+        }
+
+        System.out.println("Lägsta priset är " + minPrice);
+        System.out.println("Högsta priset är " + maxPrice);
+        System.out.println("Medelpriset är " + (Math.round(averagePrice)));
+
+    }
+
+    public static void sortering(int[] userData) {
+//        Skriv ut timmarna och priset för dessa sorterade efter billigast till dyrast pris. Ex:
+//        00-01 23 öre
+//        01-02 26 öre
+//        05-06 30 öre
+//        02-03 40 öre
+        for (int price : userData) {
+            System.out.println(userData[price] + "-" + userData[price]);
+            System.out.println(price);
+        }
+    }
+
+    public static void ladda4h() {
+//        Om man har en elbil som man vill ladda så vill man kanske göra det när priset är som billigast på
+//        dygnet. Då batteriet behöver värmas/kylas så blir det minst förluster om man genomför laddningen
+//        sammanhängande under ett antal timmar. Låt programmet hitta de 4 billigaste timmarna som ligger i
+//        följd och skriva ut vid vilket klockslag man ska börja ladda för att få lägst totalpris samt vilket
+//        medelpris det blir under dessa 4 timmar.}
+    }
+
+    public static void display() {
+//            Ett normalt konsol fönster har 80 teckens bredd och här vill vi utnyttja det för att visualisera priserna
+//            under dygnet. Lägg till ett 5:e alternativ för detta i din meny eller utöka alternativ 2 till att visa alla
+//            priser utöver min, max och medel. Exempel på hur visualiseringen kan se ut med 75 teckens bredd.
+//            Beroende på vilket prisintervall som gäller kan skalan behöva justeras dynamiskt.
+//            530|
+//                    x
+//                    |
+//                    x
+//            x
+//                    xx
+//            xx
+//                    xxxxxxxxxxxxxx
+//            xxxx
+//                    |xx|xxxxxxxxxxxxxxxxxxxxxxxxx
+//            xxxxxxxxxxxxxx
+//                    | xxxxxxxxx
+//            40| xxxxxxxxx
+//            x
+//                    |-----------------------------------------------------------------------
+//                    |00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23
+
     }
 }
